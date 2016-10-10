b2ecfd41c13952cadd5a3c489d9debd968b0e2f6
Artem Kravtsov <artemitdev@gmailcom>
2016-10-10 17:30:06 +0300

    Работа с API google

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,16 @@
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
+//
+//   setTimeout(function() {
+//     document.querySelector('.greating_picture').classList.add('m--show');
+//   }, 1000);
+// })();
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+var mapOptions = {
+  zoom: 8,
+  center: myLatlng,
+  mapTypeId: google.maps.MapTypeId.SATELLITE
+};
+var map = new google.maps.Map(document.getElementById("map"),
+    mapOptions);
