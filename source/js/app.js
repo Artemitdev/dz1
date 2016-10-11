111c0c487d92317e95741710f7bc5c6b6c83ddc4
Artem Kravtsov <artemitdev@gmailcom>
2016-10-11 11:34:44 +0300

    Реализация карты

--- source/js/app.js
+++ source/js/app.js
@@ -6,11 +6,103 @@
 //   }, 1000);
 // })();
 
-var myLatlng = new google.maps.LatLng(-34.397, 150.644);
-var mapOptions = {
-  zoom: 8,
-  center: myLatlng,
-  mapTypeId: google.maps.MapTypeId.SATELLITE
-};
-var map = new google.maps.Map(document.getElementById("map"),
-    mapOptions);
+'use strict';
+
+// Google Map
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+        scrollwheel: false,
+        styles: [
+            {
+                elementType: 'geometry',
+                stylers: [{color: '#f5f5f5'}]
+            },
+            {
+                elementType: 'labels.icon',
+                stylers: [{visibility: 'off'}]
+            },
+            {
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#616161'}]
+            },
+            {
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#f5f5f5'}]
+            },
+            {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#bdbdbd'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [{color: '#eeeeee'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#757575'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#e5e5e5'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9e9e9e'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#ffffff'}]
+            },
+            {
+                featureType: 'road.arterial',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#757575'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#dadada'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#616161'}]
+            },
+            {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9e9e9e'}]
+            },
+            {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [{color: '#e5e5e5'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [{color: '#eeeeee'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#96d7c8'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9e9e9e'}]
+            }
+        ]
+    });
+}
